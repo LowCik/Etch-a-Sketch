c49386f7dd13839b384a6945a218de38a3a72b9f
LowCik <loup.ciosek@gmail.com>
2023-04-12 22:35:06 +0200

    did 16 line but not much for now

--- scrypt.js
+++ scrypt.js
@@ -1,1 +1,23 @@
-//oui oui !! honhon
+//Initialize component
+
+const grillContainer = document.querySelector('.grill-container');
+
+/* créer un grille de 16 par 16.
+D'abord créer les 16 div "ligne"
+puis dans ces ligne, les 16 div "Case".
+  */
+function createGrill() {
+  let size = 16;
+  console.log("yes");
+  console.log("haouais");
+  for (let i = 0; i < size; i++) {
+    const line = document.createElement('div');//créer "l'objet" div
+    line.classList.add('line-start');
+    line.style.backgroundColor = 'red';
+    grillContainer.append(line);
+
+  }
+  
+  console.log('bah oausi');
+}
+createGrill();
